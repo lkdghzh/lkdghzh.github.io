9133bbf71964f6d2a56a2f2ccb5512ac895a5ab8
lkdghzh <416960628@qq.com>
2019-01-29 10:55:52 +0800

    生成文件

--- _generator/docs/.vuepress/config.js
+++ _generator/docs/.vuepress/config.js
@@ -1,19 +1,25 @@
 module.exports = {
-    title: 'Hello Unar.js',
+    title: 'Unar.js',
     description: 'Description about Unar.js',
     themeConfig: {
-        // displayAllHeaders: true, // 默认值：false
+        displayAllHeaders: true, // 默认值：false
         nav: [
             { text: 'home', link: '/' }, 
             { text: '教程', link: '/doc/' }, 
+            { text: '设计模式', link: '/design/' }, 
+            { text: '算法', link: '/algo/' }, 
             { text: '博客', link: '/blog/' }, 
             { text: 'git', link: 'https://github.com/lkdghzh' }, // 外部链接
         ],
         sidebar: {
             '/doc/': [
-                '',
-                'first',
-                'second'
+                'guide',
+            ],
+            '/algo/': [
+                'cache',
+            ],
+            '/design/': [
+                'memo',
             ],
             '/blog/': [
                 '',
